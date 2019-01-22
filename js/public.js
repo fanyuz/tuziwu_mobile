71e35eddfda3541837defe137c98ed949684b95f
fanyuz <18635665951@163.com>
2019-01-22 10:14:11 +0800

    兔子屋第一版APP Signed-off-by: fanyuz <18635665951@163.com>

--- js/public.js
+++ js/public.js
@@ -20,4 +20,22 @@ $(function(){
 	$(".header_return").click(function(){
 		window.history.back();
 	})
+	// 商品数量加减
+	$(".num_add").click(function(){
+		var _num = parseInt($(this).siblings(".num").text());
+		_num += 100;
+		$(this).prev().text(_num)
+	})
+	$(".minus").click(function(){
+		var _num = parseInt($(this).siblings(".num").text());
+		if(_num > 1){
+			_num -=100;
+		}else{
+			_num = 0;
+		}
+		$(this).next().text(_num)
+	});
+	
+	
 })
+
